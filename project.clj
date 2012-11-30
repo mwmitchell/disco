040b6533ce50ef9deeb1a9f04a208f60e300f3ed
Matt Mitchell <goodieboy@gmail.com>
2012-11-30 17:17:49 -0500

    fixed test dir/path problem

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject disco "1.0.0-SNAPSHOT"
   :description "Disco - Solr toolbox"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-time "0.3.1"]]
+                 [clj-time "0.4.4"]]
   :profiles {:dev {:dependencies [[midje "1.4.0"]
                                   [bultitude "0.2.0"]]}})
